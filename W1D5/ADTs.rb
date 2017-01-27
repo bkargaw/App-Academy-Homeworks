24d25bd51345d805e5a95c4d7b4fa27d06a19e09
Bruk Argaw <bkargaw@gmail.com>
2017-01-27 00:33:55 -0800

    implemented a satck

---
+++ W1D5/ADTs.rb
@@ -0,0 +1,19 @@
+class Stack
+  def initialize
+    @stack = []
+  end
+
+  def add(el)
+    @stack.push(el)
+  end
+
+  def remove
+    @stack.pop
+  end
+
+  def show
+    @stack.dup
+  end
+end
+
+cle
