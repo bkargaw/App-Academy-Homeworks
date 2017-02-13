7119790c394731fb892aba239e7e0ef53f048bcc
Bruk Argaw <bkargaw@gmail.com>
2017-02-13 01:43:48 -0800

    finished the controller logic for the book hw

--- W4/W4D1/library/app/controllers/books_controller.rb
+++ W4/W4D1/library/app/controllers/books_controller.rb
@@ -1,18 +1,24 @@
 class BooksController < ApplicationController
   def index
     # your code here
+    @books = Book.all
   end
 
   def new
     # your code here
+    render 'new'
   end
 
   def create
     # your code here
+    Book.create!(book_params)
+    redirect_to books_url
   end
 
   def destroy
     # your code here
+    Book.find_by(id: params[:id]).destroy
+    redirect_to books_url
   end
 
   private
