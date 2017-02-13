4998a82a1844b8a3a3d585d65eaced430ff68b5b
Bruk Argaw <bkargaw@gmail.com>
2017-02-13 00:14:06 -0800

    initalized rails project

--- W3/W3D4/n_1_buster/app/models/plant.rb
+++ W3/W3D4/n_1_buster/app/models/plant.rb
@@ -15,10 +15,10 @@ class Plant < ActiveRecord::Base
   primary_key: :id
 )
 
-has_many(
-  :seeds,
-  class_name: "Seed",
-  foreign_key: :plant_id,
-  primary_key: :id
-)
+# has_many(
+#   :seeds,
+#   class_name: "Seed",
+#   foreign_key: :plant_id,
+#   primary_key: :id
+# )
 end
